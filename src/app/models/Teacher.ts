920faa2b0f3a1422ab46cd76da81a1f4db2d9745
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-04-23 10:16:15 -0400

    Teacher service

---
+++ src/app/models/Teacher.ts
@@ -0,0 +1,7 @@
+export interface Teacher{
+  TeacherId: number,
+  Name: string,
+  LastName: string,
+  Email: string,
+  Skills: string
+}
---
+++ src/app/services/teacher.service.ts
@@ -0,0 +1,38 @@
+import { Teacher } from './../models/Teacher';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeacherService {
+
+  constructor(private http: HttpClient) { }
+
+  private apiURL:string = "https://localhost:44344/api";
+
+
+  createTeacher(teacher: Teacher): Observable<Teacher>{
+    const fullURL = `${this.apiURL}/Teacher/PostTeacher`;
+    return this.http.post<Teacher>(fullURL,teacher);
+  }
+
+  editTeacher(teacher: Teacher): Observable<Teacher>{
+    const fullURL = `${this.apiURL}/Teacher/UpdateCourse`;
+    return this.http.put<Teacher>(fullURL,teacher);
+  }
+  deleteTeacher(id: number){
+    const fullURL = `${this.apiURL}/Teacher/RemoveTeacher/${id}`;
+    return this.http.delete<Teacher>(fullURL);
+  }
+
+  getTeacher(id: number){
+    const fullURL = `${this.apiURL}/Teacher/GetTeacher/${id}`;
+    return this.http.get<Teacher>(fullURL);
+  }
+  getAllTeacher(){
+    const fullURL = `${this.apiURL}/Teacher/GetTeachers`;
+    return this.http.get<Teacher>(fullURL);
+  }
+}
