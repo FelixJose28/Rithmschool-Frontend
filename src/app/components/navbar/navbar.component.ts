1f7fd47ec8856530bf11865b6444e8d14fe16789
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-04-23 09:42:32 -0400

    navbar by userRol

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,17 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Role } from './../../models/Role';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { User } from 'src/app/models/User';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-
-  constructor() { }
+export class NavbarComponent implements OnInit, OnChanges {
+  userRol?: number;
+  constructor(
+    private router: Router,
+    private auth: AuthenticationService,
+    private route: ActivatedRoute) { }
 
 
   ngOnInit(): void {
+    this.auth.getCurrentUser()
+    .then((userLogged)=> this.auth.getRol()
+      .then((userRolLogged)=>
+        {
+          this.userRol=userRolLogged;
+          console.log(this.userRol)}
+    ));
     this.showHide();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
+    //Add '${implements OnChanges}' to the class.
+    this.auth.getCurrentUser()
+    .then((userLogged)=> this.auth.getRol()
+      .then((userRolLogged)=>
+        {
+          this.userRol=userRolLogged;
+          console.log(this.userRol)}
+    ));
   }
 
   showHide(){
@@ -23,7 +48,14 @@ export class NavbarComponent implements OnInit {
         navbarLink[i].classList.toggle('activate')
       }
     });
-    
+
+  }
+  logout(){
+    this.auth.logout();
+    this.router.navigateByUrl('/shared/home');
+  }
+  get Roles() {
+    return Role;
   }
 
 }
