ab43beb61b3148d57af18c44715d32fee7219502
Felix Jose <felixjoseynfantequezada@gmail.com>
2021-04-24 18:40:35 -0400

    update a teacher

--- src/app/models/Teacher.ts
+++ src/app/models/Teacher.ts
@@ -5,3 +5,4 @@ export interface Teacher{
   email: string,
   skills: string
 }
+
--- src/app/pages/admin/teacher/update/update.component.ts
+++ src/app/pages/admin/teacher/update/update.component.ts
@@ -1,7 +1,9 @@
+import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { TeacherService } from 'src/app/services/teacher.service';
+import {Teacher} from '../../../../models/Teacher'
 
 @Component({
   selector: 'app-update',
@@ -16,21 +18,53 @@ export class UpdateComponent implements OnInit {
     Email: [''],
     Skills: ['']
   })
+  teacherCurrenData?: Teacher;
+  teacherNew?:Teacher;
+  teacherId: any = this.activatedroute.snapshot.paramMap.get("id");;
 
   constructor(
     private fb: FormBuilder,
     private teacherService: TeacherService,
-    private activatedroute:ActivatedRoute) { }
+    private activatedroute:ActivatedRoute,
+    private location: Location) { }
+
 
   ngOnInit(): void {
+    this.getTeacher();
   }
-  createTeacher(){
+  getTeacher(){
+    this.teacherId = this.activatedroute.snapshot.paramMap.get("id");
+    this.teacherService.getTeacher(this.teacherId).subscribe({
+      next: (teacherResponse)=>{
+        this.teacherCurrenData = teacherResponse;
+      },
+      error:(x)=>console.log(x)
+    })
+  }
+  goBack(): void{
+    this.location.back();
+    console.log(this.teacherId)
+  }
+  updateTeacher(){
     const teacher = this.contactForm.value;
     console.log(teacher)
     this.teacherService.updateTeacher(teacher).subscribe({
-      next: (x)=>console.log(x),
-      error: (err)=>console.log(err)
-    });
+      next:(x)=>{
+        console.log(x)
+      },
+      error:(x)=>{
+        console.log(x)
+      }
+    })
+  // this.teacherService.updateTeacher(teacher).subscribe({
+  //   next: (teacherIdResponse)=>
+  //     {
+  //       this.teacherService.getTeacher(2)
+  //     console.log(this.teacherId)
+
+  //   },
+  //   error: (err)=>console.log(err)
+  // });
   }
 
 }
